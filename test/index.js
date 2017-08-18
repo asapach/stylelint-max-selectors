44a4917816667a5dff2496650c4590cf3470586f
Aliaksei Sapach <dreamsonic@tut.by>
2017-08-18 19:10:50 +0300

    implemented the rule and added tests

---
+++ test/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const maxSelectors = require("../lib");
+const testRule = require("stylelint-test-rule-tape");
+
+testRule(maxSelectors.rule, {
+  ruleName: maxSelectors.ruleName,
+  config: [2],
+
+  accept: [
+    { code: ".foo {} .bar {}" },
+    { code: ".foo .bar {}" },
+    { code: ".foo, .bar {}" },
+  ],
+
+  reject: [
+    {
+      code: ".foo, .bar, .baz {}",
+      message: `Expected no more than 2 selectors (${maxSelectors.ruleName})`
+    },
+    {
+      code: ".foo {} .bar {} .baz {}",
+      message: `Expected no more than 2 selectors (${maxSelectors.ruleName})`
+    },
+  ],
+});
